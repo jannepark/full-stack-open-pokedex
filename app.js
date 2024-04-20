7ae24e374712b71a002f707f11cac33b1b602eab
n8761 <n8761@student.jamk.fi>
2024-04-20 15:38:18 +0000

    testing health check when error, errors removed, should deploy, task 11.11

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.listen(PORT, () => {
 
 app.get('/health', (req, res) => {
   // throw 'error...'
+  // eslint-disable-next-line no-constant-condition
   if (true) throw 'error...  '
   res.send('ok')
 })
